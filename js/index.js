9a0b5016c22f87a087788a451b7e0433d99b189d
Flash <flash.jaq@gmail.com>
2018-09-14 23:45:52 +0100

    Added createAccount function

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
 /*
 * Author: Flash
-* Date: 13/09/2018
+* Date: 14/09/2018
 */
 "use strict";
 
 let web3;
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+console.log("Generated new web3 provider");
+console.log("web3.version: " + web3.version + "\n");
+
+createAccount();
+
+async function createAccount() {
+	var key = await web3.eth.accounts.create();
+	console.log("privateKey: " + key.privateKey);
+	console.log("address: " + key.address);
+}
