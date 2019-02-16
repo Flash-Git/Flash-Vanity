4e5633bc4f556d24bf25ef8441f0d44a7f2e149d
Flash <flash.jaq@gmail.com>
2019-02-16 06:06:24 +0000

    Added yargs cli help

--- Generator/index.js
+++ Generator/index.js
@@ -1,7 +1,20 @@
 #!/usr/bin/env node
 const crypto = require("crypto");
 const ethUtils = require("ethereumjs-util");
-const argv = require('yargs').argv;
+const argv = require("yargs")
+      .usage("Usage: $0 -n [num] -s [string] -p [num]")
+      .demandOption(["s"])
+      .default("n", 100)
+      .alias("n", "number")
+      .describe("n", "Number of matching addresses to generate")//find is more accurate
+      .alias("s", "string")//TODO change to smth less dumb
+      .describe("s", "String to find in addresses, supports multiple strings seperated by commas")
+      .describe("p", "Minimum number of required strings to find in each address")
+      .example("$0 -s '1337' | Finds addresses containing '1337'")
+      .example("$0 -s '1337, b00b5' | Finds addresses containing either '1337' or 'b00b5'")
+      .example("$0 -n 50 -s '1337' | Finds 50 addresses containing '1337'")
+      .example("$0 -s '1337, b00b5' -p '2' | Finds addresses containing both '1337' and 'b00b5'")
+      .argv;
 
 //const filteredAdds = [];
 
