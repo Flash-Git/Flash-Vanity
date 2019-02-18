4ec5e2a187bf34ab0f2c3ef3dbbcc3e234c22073
Flash <flash.jaq@gmail.com>
2019-02-18 21:13:41 +0000

    Added styling and an import button

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -9,8 +9,12 @@ class Form extends Component {
   state = {
   }
   
-  onClick = (e) => {
-    this.generateAccounts(50);
+  onClickGenerate = (e) => {
+    this.generateAccounts(20);
+  }
+  
+  onClickImport = (e) => {
+    this.importAccounts();
   }
 
   generateAccounts = (_num) => {
@@ -27,10 +31,16 @@ class Form extends Component {
     return { address, privKey: privKey.toString("hex") };
   }
 
+  importAccounts = () => {
+
+  }
+
   render(){
     return(
       <div id="section-form" className="section" style={ formStyle }>
-        <button onClick={ this.onClick } >Generate</button>
+        <button onClick={ this.onClickGenerateRandom } style={ btnStyle }>Generate Random</button>
+        <button onClick={ this.onClickImport } style={ btnStyle }>Import</button>
+        {/* Display Form that updates a command string to get desired flash-vanity address out of generator  */}
       </div>
     );
   }
@@ -43,6 +53,19 @@ const formStyle = {
   margin: "1rem",
   background: "#565656",
   color: "#fff"
+}
+
+const btnStyle = {
+  background: "#666482",
+  padding: "0.8rem 1rem",
+  border: "none",
+  borderRadius: "3%",
+  color: "#fff",
+  fontWeight: "bold",
+  cursor: "pointer",
+  margin: "1rem",
+  marginRight: "3rem",
+  minWidth: "10rem"
 }
 
 //PropTypes
