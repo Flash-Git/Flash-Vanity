d006014362b19883e39718d0cdf65935ab3ab6b3
Flash <flash.jaq@gmail.com>
2019-08-30 23:26:53 +0200

    Added page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-import Header from "./components/Header";
-import Listing from "./components/Listing";
-import Form from "./components/Form";
+import Home from "./components/pages/Home";
+
+import AccountState from "./context/account/AccountState";
 
 import "./App.css";
 
 const App = () => (
   <div className="App">
-    <Header />
-    <Form />
-    <Listing />
+    <AccountState>
+      <Home />
+    </AccountState>
   </div>
 );
 
---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+
+import Header from "../Header";
+import Listing from "../Listing";
+import Form from "../Form";
+
+const Home = () => (
+  <Fragment>
+    <Header />
+    <Form />
+    <Listing />
+  </Fragment>
+);
+
+export default Home;
