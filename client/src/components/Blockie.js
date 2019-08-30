9dcd06324b89d9acb76746634f71ca06b244be8e
Flash <flash.jaq@gmail.com>
2019-08-30 22:29:25 +0200

    Removed return

--- client/src/components/Blockie.js
+++ client/src/components/Blockie.js
@@ -1,19 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import makeBlockie from "ethereum-blockies-base64";
 import PropTypes from "prop-types";
 
-const Blockie = ({ address, size }) => {
-  return (
-    <img
-      src={makeBlockie(address)}
-      width={size}
-      height={size}
-      alt="blockie"
-      style={blockieStyle}
-    />
-  );
-};
-
+const Blockie = ({ address, size }) => (
+  <img
+    src={makeBlockie(address)}
+    width={size}
+    height={size}
+    alt="blockie"
+    style={blockieStyle}
+  />
+);
 const blockieStyle = {
   display: "block",
   marginLeft: "auto",
