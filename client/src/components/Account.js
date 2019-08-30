feb9f4e084bc34e12cdf42bf47ec4170ba3f1180
Flash <flash.jaq@gmail.com>
2019-08-30 23:01:33 +0200

    Removed style

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -11,42 +11,20 @@ const Account = ({ account }) => {
   };
 
   return (
-    <div className="generatedAdd" style={generatedAddStyle}>
-      <div style={{ width: "2em", float: "left", margin: "0.1rem" }}>
-        <Blockie address={account.address} size="30px" />
-      </div>
-      <div
-        style={{
-          width: "22em",
-          float: "left",
-          marginLeft: "0.2rem",
-          borderRight: "solid"
-        }}
-      >
-        <a href={`https://etherscan.io/address/ ${account.address}`}>
-          {account.address}
-        </a>
-      </div>
-      {open ? (
-        <div style={{ width: "34em", float: "left", marginLeft: "0.2rem" }}>
-          {account.privKey}
-        </div>
-      ) : (
-        <button onClick={onClick}>Show Private Key</button>
-      )}
-      <div style={{ width: "2em", float: "left", margin: "0.1rem" }}>
-        <Blockie address={account.address} size="30px" />
+    <div className="generatedAdd">
+      <Blockie address={account.address} size="30px" />
+      <a href={`https://etherscan.io/address/ ${account.address}`}>
+        {account.address}
+      </a>
+      <div>
+        {open ? (
+          <span>{account.privKey}</span>
+        ) : (
+          <button onClick={onClick}>Show Private Key</button>
+        )}
       </div>
     </div>
   );
-};
-
-const generatedAddStyle = {
-  display: "flex",
-  justifyContent: "center",
-  background: "#49494B",
-  color: "#fff",
-  margin: "0.4rem"
 };
 
 //PropTypes
