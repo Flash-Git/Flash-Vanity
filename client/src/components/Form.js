fb6cf85d1079cd9d66918984d2426a92f983dbba
Flash <flash.jaq@gmail.com>
2019-08-30 23:27:27 +0200

    Cleaned code

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,9 +1,25 @@
-import React, { Component, useState, useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import crypto from "crypto";
 import { utils } from "ethers";
-import PropTypes from "prop-types";
 
-const Form = ({ setAccounts }) => {
+import AccountContext from "../context/account/AccountContext";
+
+const Form = () => {
+  const accountContext = useContext(AccountContext);
+
+  const { addAccount } = accountContext;
+
+  const generateAccounts = _num => {
+    for (let i = 0; i < _num; i++) {
+      addAccount(getNewAccount());
+    }
+  };
+
+  const getNewAccount = () => {
+    const privKey = crypto.randomBytes(32);
+    const address = "0x" + utils.computeAddress(privKey).toString("hex");
+    return { address, privKey: privKey.toString("hex") };
+  };
   const onClickGenerate = e => {
     generateAccounts(20);
   };
@@ -12,60 +28,15 @@ const Form = ({ setAccounts }) => {
     importAccounts();
   };
 
-  const generateAccounts = _num => {
-    const accounts = [];
-    for (let i = 0; i < _num; i++) {
-      accounts.push(getNewAccount());
-    }
-    setAccounts(accounts);
-  };
-
-  const getNewAccount = () => {
-    const privKey = crypto.randomBytes(32);
-    const address = "0x" + utils.computeAddress(privKey).toString("hex");
-    return { address, privKey: privKey.toString("hex") };
-  };
-
   const importAccounts = () => {};
 
   return (
-    <div id="section-form" className="section" style={formStyle}>
-      <button onClick={onClickGenerate} style={btnStyle}>
-        Generate Random
-      </button>
-      <button onClick={onClickImport} style={btnStyle}>
-        Import
-      </button>
+    <div id="section-form" className="section">
+      <button onClick={onClickGenerate}>Generate Random</button>
+      <button onClick={onClickImport}>Import</button>
       {/* Display Form that updates a command string to get desired flash-vanity address out of generator  */}
     </div>
   );
-};
-
-const formStyle = {
-  textAlign: "center",
-  justifyContent: "center",
-  padding: "1rem",
-  margin: "1rem",
-  background: "#565656",
-  color: "#fff"
-};
-
-const btnStyle = {
-  background: "#666482",
-  padding: "0.8rem 1rem",
-  border: "none",
-  borderRadius: "3%",
-  color: "#fff",
-  fontWeight: "bold",
-  cursor: "pointer",
-  margin: "1rem",
-  marginRight: "3rem",
-  minWidth: "10rem"
-};
-
-//PropTypes
-Form.propTypes = {
-  setAccounts: PropTypes.func.isRequired
 };
 
 export default Form;
