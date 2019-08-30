cf34e96b4f2c991bdbbfe07ec9f4136969bdafbc
Flash <flash.jaq@gmail.com>
2019-08-30 23:27:13 +0200

    Removed styling

--- client/src/components/Listing.js
+++ client/src/components/Listing.js
@@ -1,28 +1,20 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useContext } from "react";
 
 import Account from "./Account";
 
-const Listing = ({ accounts }) => (
-  <div id="section-listing" className="section" style={listingStyle}>
-    {accounts.map(account => (
-      <Account key={account.address} account={account} />
-    ))}
-  </div>
-);
+import AccountContext from "../context/account/AccountContext";
 
-const listingStyle = {
-  textAlign: "center",
-  justifyContent: "center",
-  padding: "1rem",
-  margin: "1rem",
-  background: "#565656",
-  color: "#fff"
+const Listing = () => {
+  const accountContext = useContext(AccountContext);
+
+  const { accounts } = accountContext;
+
+  return (
+    <div id="section-listing" className="section">
+      {accounts.map(account => (
+        <Account key={account.address} account={account} />
+      ))}
+    </div>
+  );
 };
-
-//PropTypes
-Listing.propTypes = {
-  accounts: PropTypes.array.isRequired
-};
-
 export default Listing;
